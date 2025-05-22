d8a7472fad369a2a487f655f202420c2eb17097d
Brian Barnes <houdinii1984@gmail.com>
2025-05-21 19:42:37 -0600

    Drop in-repo modules in favor of submodules

--- modules/houdinii_gui/visualizers/WaveShapeVisualizerComponent.cpp
+++
@@ -1,4 +0,0 @@
-#include "WaveShapeVisualizerComponent.h"
-
-// The implementation is already in the header file as inline methods
-// This file can be empty or contain any non-inline implementations
